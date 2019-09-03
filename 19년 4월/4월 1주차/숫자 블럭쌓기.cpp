a7146490f7c29693bfa8e5a836dec40571efabb4
Jeon Ga Bin <hyanghope@naver.com>
2019-09-04 01:14:35 +0900

    숫자블록 수정중2

--- 19년 4월/4월 1주차/숫자 블럭쌓기.cpp
+++ 19년 4월/4월 1주차/숫자 블럭쌓기.cpp
@@ -2,18 +2,25 @@
 using namespace std;
 int minarr[202]={0,};
 int maxarr[202]={0,};
+int arr1[202]={0,};
+int arr2[202]={0,};
+int maxres[202]={0,};
+int minres[202]={0,};
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int n,m;
     cin>>n>>m;
     for(int i=0;i<m;i++){
-        int a,b;
-        cin>>a>>b;
-        minarr[a]++;
-        maxarr[b]++;
+        cin>>arr1[i]>>arr2[i];
+        minarr[arr1[i]]++;
+        maxarr[arr2[i]]++;
+    }
+    for(int i=0;i<m;i++){
+        minres[arr1[i]]+=(1+minarr[arr2[i]]);
+        maxres[arr2[i]]+=(1+maxarr[arr1[i]]);
     }
     for(int i=1;i<=n;i++){
-        cout<<minarr[i]<<" "<<maxarr[i]<<"\n";
+        cout<<maxres[i]<<" "<<minres[i]<<"\n";
     }
 }
